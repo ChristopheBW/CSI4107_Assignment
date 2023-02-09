3bd658c783ed7239ce46ef799bd2ba082e0ff0e1
Bill729 <s11050003@gmail.com>
2023-02-08 22:05:46 -0500

    Step (3) changes
    1. Retrieval and Ranking part is implemented in Indexer.py
    
    2. 50 test queries needs to be implemented.

--- A1/Indexer.py
+++ A1/Indexer.py
@@ -1,6 +1,6 @@
 import math
 import Tokenizer # Path: ./Tokenizer.py
-
+from collections import defaultdict
 
 def tf_idf(tokenized_docs):
     '''Calculates the tf-idf weight for each term in each document
@@ -57,6 +57,68 @@ def tf_idf(tokenized_docs):
     return tf_idf
 
 
+def getCosineSimilarity(query, tf_idf):
+
+    '''Calculates the cosine similarity of each indexed documents as query words are processed one by one.
+
+    :param query: the query in text
+    :type query: list
+    :param tf_idf
+    :type tf_idf: dict
+    :return: ranked list of documented in reversed order of their relevance.
+    :rtype: list
+    '''
+
+    # Finds the number of documents that contain one or more query words.
+    # The list then contains the documents that have the one of the query word.
+    docContain = []
+    for term in query:
+        if term in tf_idf:
+            for i in range(len(tf_idf[term])):
+                if tf_idf[term][i] > 0:
+                    docContain = list(set(docContain + [i]))
+
+
+    # Calculates the cosine similarity of each indexed documents and the query words.
+    # Incrementally computes cosine similarity of each indexed documents as query words are
+    # processed one by one.
+    cosSimilarity = {}
+    
+    for doc in docContain:
+        cosSimilarity[doc] = 0
+        querytf_idf = 0
+        doctf_idf = 0
+        for term in query:
+            if term in tf_idf:
+                querytf_idf += tf_idf[term][doc]
+                doctf_idf += math.pow(tf_idf[term][doc], 2)
+
+        sumQ = 0.0
+        for term in tf_idf:
+            sumQ += math.pow(tf_idf[term][doc],2)
+
+        queryMagnitude = math.sqrt(sumQ)
+        docMagnitude = math.sqrt(doctf_idf)
+        
+        # Result measurement should be values that are bound by a constrained range of 0 and 1.
+        cosSimilarity[doc] = querytf_idf / (queryMagnitude * docMagnitude)
+        
+    print(cosSimilarity)
+        
+    # Sorts the result based on the value of cosine similarity.
+    # Returns the documents in descending order of their relevance.
+    # It is ordered in reverse order of their relevance.
+    relevantDoc = list(cosSimilarity.items())
+    relevantDoc.sort(key = lambda a: a[1], reverse = True)
+    
+    # List that is ranked contains the ids of the relevant document.
+    rankedList = []
+    for id in relevantDoc:
+        rankedList.append(id[0])
+
+    return rankedList
+
+
 # TODO: Delete the following code before the submission
 # The following code is for testing
 if __name__ == '__main__':
@@ -66,9 +128,12 @@ if __name__ == '__main__':
         "From its beginning as a Hollywood talent-booking agency, MCA has grown into a powerhouse in producing movies, television programming, records and toys."
     ]
     tokenized_docs = [Tokenizer.tokenize(doc) for doc in docs]
-    for doc in docs:
-        print(doc)
-    print("\nTokenized docs:\n", tokenized_docs)
+    #for doc in docs:
+        #print(doc)
 
-    print("\ntf-idf:\n", tf_idf(tokenized_docs))
+    #print("\ntf-idf:\n", tf_idf(tokenized_docs))
+    query = Tokenizer.tokenize("Wasserman's control comes more from the respect he commands than from the stock he controls.")
+    print(getCosineSimilarity(query,tf_idf(tokenized_docs)))
 
+    
+            
