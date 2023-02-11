0803a281e36bc7de26abb582a6c5a21ab0ca136d
ChristopheBW <bzeng087@uottawa.ca>
2023-02-11 11:00:08 -0500

    Optimized memory use of tf-idf, adapted cosine_similarity by Bystander to the current architecture

--- A1/src/IRSystem.py
+++ A1/src/IRSystem.py
@@ -1,6 +1,4 @@
 import math
-import re  # regular expression to filter out punctuations
-import multiprocessing
 import Extractor
 
 
@@ -13,15 +11,6 @@ class IRSystem:
         self.N = len(self.collection)
         self.inverted_index = {}
         self.load_tf_idf()
-
-    # def get_inverted_index(self):
-    #     """ get the inverted index
-    #
-    #     :return: the inverted index
-    #     :rtype: dict
-    #     """
-    #
-    #     return self.inverted_index
 
     # tf-idf implementation by "GEGEFE"
     def getInvertedIndex(self, tokens1):
@@ -79,17 +68,17 @@ class IRSystem:
             # tf_df: a hashmap of term frequency in each document and document frequency
             #   structure: term -> {docno -> tf, ... , "df" -> df}
             #   example: "the" -> {d0 -> 2, d1 -> 1, d2 -> 3, "df" -> 6}
-            tf_df = {}
+            self.inverted_index = {}
 
             for docno, tokens in self.collection.items():
                 for token in tokens:
-                    if token not in tf_df:
-                        tf_df[token] = {"df": 0}
-                    if docno not in tf_df[token]:
-                        tf_df[token][docno] = 1
+                    if token not in self.inverted_index:
+                        self.inverted_index[token] = {"df": 0}
+                    if docno not in self.inverted_index[token]:
+                        self.inverted_index[token][docno] = 1
                     else:
-                        tf_df[token][docno] += 1
-                    tf_df[token]["df"] += 1
+                        self.inverted_index[token][docno] += 1
+                    self.inverted_index[token]["df"] += 1
 
             print("tf_df done")
             # print('tf_df: ', tf_df)
@@ -97,24 +86,33 @@ class IRSystem:
             # tf-idf: a hashmap of tf-idf weight for each term in each document
             #   structure: term -> {docno -> tf-idf}
             #   example: "the" -> {d0 -> 0.1, d1 -> 0.05, d2 -> 0.15}
-            self.inverted_index = {}
-            for term in tf_df:
+            for term in self.inverted_index:
+                term_map = self.inverted_index[term]
                 self.inverted_index[term] = {}
-                for docno in tf_df[term]:
-                    if docno == "df":
-                        continue
-                    self.inverted_index[term][docno] = tf_df[term][docno] * math.log2(
-                        self.N / tf_df[term]["df"])
+                for docno in term_map:
+                    if docno != "df":
+                        self.inverted_index[term][docno] = term_map[docno] * math.log2(
+                            self.N / term_map["df"])
 
             print("tf_idf done")
-        else: # load from file
+        else:  # load from file
             with open(file_path, "r") as f:
                 for line in f:
                     line = line.strip()
                     term, docnos = line.split(":")
                     self.inverted_index[term] = {}
                     for docno in docnos.split():
-                        self.inverted_index[term][docno] = float(docnos.split()[1])
+                        self.inverted_index[term][docno] = float(
+                            docnos.split()[1])
+
+    def get_inverted_index(self):
+        """ get the inverted index
+
+        :return: the inverted index
+        :rtype: dict
+        """
+
+        return self.inverted_index
 
     def save_inverted_index(self, file_path):
         """ save the inverted index to a file
@@ -128,7 +126,8 @@ class IRSystem:
             for term in self.inverted_index:
                 f.write(term + ": ")
                 for docno in self.inverted_index[term]:
-                    f.write(docno + " " + str(self.inverted_index[term][docno]) + " ")
+                    f.write(docno + " " +
+                            str(self.inverted_index[term][docno]) + " ")
                 f.write("\n")
 
     def get_cosine_similarity(self, query):
@@ -139,9 +138,62 @@ class IRSystem:
         :return: ranked list of documented in reversed order of their relevance.
         :rtype: list
         '''
+        
+        # RUNS SLOWLY: stucked here more than 1 minute in my computer (i9-12900K)
+        # # Finds the number of documents that contain one or more query words.
+        # # The list then contains the documents that have the one of the query word.
+        # docContain = set()
+        # for term in query:
+        #     if term in self.inverted_index:
+        #         for docno in self.inverted_index[term]:
+        #             docContain.add(docno)
+        #
+        #
+        # # Calculates the cosine similarity of each indexed documents and the query words.
+        # # Incrementally computes cosine similarity of each indexed documents as query words are
+        # # processed one by one.
+        # cosSimilarity = {}
+        #
+        # for doc in docContain:
+        #     cosSimilarity[doc] = 0
+        #     querytf_idf = 0
+        #     doctf_idf = 0
+        #     for term in query:
+        #         if term in self.inverted_index:
+        #             weight = self.inverted_index[term][doc] if doc in self.inverted_index[term] else 0
+        #             querytf_idf += weight
+        #             doctf_idf += math.pow(weight, 2)
+        #
+        #     sumQ = 0.0
+        #     for term in self.inverted_index:
+        #         weight = self.inverted_index[term][doc] if doc in self.inverted_index[term] else 0
+        #         sumQ += math.pow(weight,2)
+        #
+        #     queryMagnitude = math.sqrt(sumQ)
+        #     docMagnitude = math.sqrt(doctf_idf)
+        #
+        #     # Result measurement should be values that are bound by a constrained range of 0 and 1.
+        #     cosSimilarity[doc] = querytf_idf / (queryMagnitude * docMagnitude)
+        #
+        # print(cosSimilarity)
+        #
+        # # Sorts the result based on the value of cosine similarity.
+        # # Returns the documents in descending order of their relevance.
+        # # It is ordered in reverse order of their relevance.
+        # relevantDoc = list(cosSimilarity.items())
+        # relevantDoc.sort(key = lambda a: a[1], reverse = True)
 
-        # calculate the query vector
+        # List that is ranked contains the ids of the relevant document.
+        # rankedList = []
+        # for id in relevantDoc:
+        #     rankedList.append(id[0])
+        #
+        # return rankedList
+
         print(query)
+        # calculate the query vector
+        #   structure: term -> tf
+        #   example: "the" -> 2
         query_vector = {}
         for term in query:
             if term not in query_vector:
@@ -150,13 +202,16 @@ class IRSystem:
                 query_vector[term] += 1
 
         # calculate the document vector
+        #   structure: docno -> {term -> tf-idf}
+        #   example: d0 -> {"the" -> 0.1, "is" -> 0.05}
         document_vectors = {}
         for term in query_vector:
             if term in self.inverted_index:
                 for docno in self.inverted_index[term]:
                     if docno not in document_vectors:
                         document_vectors[docno] = {}
-                    document_vectors[docno][term] = self.inverted_index[term][docno]
+                    document_vectors[docno][term] = self.inverted_index[term][
+                        docno]
 
         # calculate the cosine similarity
         cosine_similarities = {}
@@ -164,16 +219,21 @@ class IRSystem:
             cosine_similarities[docno] = 0
             for term in query_vector:
                 if term in document_vectors[docno]:
-                    cosine_similarities[docno] += query_vector[term] * document_vectors[docno][term]
+                    cosine_similarities[docno] += query_vector[
+                        term] * document_vectors[docno][term]
 
         # normalize the cosine similarity
         for docno in cosine_similarities:
-            cosine_similarities[docno] /= math.sqrt(len(query_vector) * len(document_vectors[docno]))
+            cosine_similarities[docno] /= math.sqrt(
+                len(query_vector) * len(document_vectors[docno]))
 
         # sort the cosine similarity
-        cosine_similarities = sorted(cosine_similarities.items(), key=lambda x: x[1], reverse=True)
+        cosine_similarities = sorted(cosine_similarities.items(),
+                                     key=lambda x: x[1],
+                                     reverse=True)
 
         return cosine_similarities[:50]
+
 
 if __name__ == '__main__':
     irs = IRSystem("./collection.txt", "./topics1-50.txt")
