2ea714f4ec3ca79ba5ab8ef21e6dadaabbc2f682
ChristopheBW <bzeng087@uottawa.ca>
2023-04-09 16:05:59 -0400

    BERT like model finished

---
+++ A2/src/BERT_IRSystem.py
@@ -0,0 +1,387 @@
+import math
+import time
+import multiprocessing
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from multiprocessing.pool import ThreadPool as Pool
+import torch
+from torch import nn
+from tqdm import tqdm
+from scipy.spatial.distance import cosine
+import operator
+from transformers import RobertaTokenizer, RobertaModel
+import Extractor
+
+class IRSystem:
+
+    def __init__(self, collection_path, query_path):
+        self.extractor = Extractor.Extractor(collection_path, query_path)
+        self.collection = self.extractor.get_collection()
+        self.queries = self.extractor.get_queries()
+        self.N = len(self.collection)
+        self.Q = len(self.queries)
+
+        self.inverted_index = {}
+        # self.load_tf_idf()
+
+    # tf-idf implementation by "GEGEFE"
+    def getInvertedIndex(self, tokens1):
+        '''
+        get the inverted index
+        :param tokens: the tokenized text (list)
+        :return: the inverted index (dict)
+        '''
+
+        tokens2 = [tokens1]
+        inverted_index, counters = {}, {
+        }  # use hashmap to store the inverted index
+
+        # iter through the tokens
+        for i, tokens in enumerate(tokens2):
+            for token in tokens:
+
+                if token not in inverted_index:
+                    inverted_index[token] = [i]
+                else:
+                    inverted_index[token].append(i)
+                if token not in counters:
+                    counters[token] = 1
+                else:
+                    counters[token] += 1
+
+        inverse_document_frequency = {}
+        for word, document_indices in inverted_index.items():
+            inverse_document_frequency[word] = math.log(
+                len(tokens2) / len(document_indices))
+        
+
+        weights = {}
+        for document_index, tokens in enumerate(tokens2):
+            for word in tokens:
+                term_frequency = tokens.count(word) / len(tokens)
+                weights[(word, document_index
+                         )] = term_frequency * inverse_document_frequency[word]
+        
+        return inverted_index, weights
+
+    # tf-idf implementation by "NullPointer"
+    def load_tf_idf(self, file_path=None):
+        '''Calculates the tf-idf weight for each term in each document with multiprocessing.
+        O(N * M) where N is the number of documents and M is the avg number of terms
+
+        :param file_path: the file path to load the inverted index
+        :type file_path: str
+        :return: the hashmap of tf-idf weight for each term in each document
+        :rtype: dict
+        '''
+
+        if file_path is None:
+            # tf_df: a hashmap of term frequency in each document and document frequency
+            #   structure: term -> {docno -> tf, ... , "df" -> df}
+            #   example: "the" -> {d0 -> 2, d1 -> 1, d2 -> 3, "df" -> 6}
+            self.inverted_index = {}
+
+            for docno, tokens in self.collection.items():
+                for token in tokens:
+                    if token not in self.inverted_index:
+                        self.inverted_index[token] = {"df": 0}
+                    if docno not in self.inverted_index[token]:
+                        self.inverted_index[token][docno] = 1
+                    else:
+                        self.inverted_index[token][docno] += 1
+                    self.inverted_index[token]["df"] += 1
+
+            print("tf_df done")
+            # print('tf_df: ', tf_df)
+
+            # tf-idf: a hashmap of tf-idf weight for each term in each document
+            #   structure: term -> {docno -> tf-idf}
+            #   example: "the" -> {d0 -> 0.1, d1 -> 0.05, d2 -> 0.15}
+            for term in self.inverted_index:
+                term_map = self.inverted_index[term]
+                self.inverted_index[term] = {}
+                for docno in term_map:
+                    if docno != "df":
+                        self.inverted_index[term][docno] = term_map[
+                            docno] * math.log2(self.N / term_map["df"])
+
+            print("tf_idf done")
+        else:  # load from file
+            with open(file_path, "r") as f:
+                for line in f:
+                    line = line.strip()
+                    term, docnos = line.split(":")
+                    self.inverted_index[term] = {}
+                    for docno in docnos.split():
+                        self.inverted_index[term][docno] = float(
+                            docnos.split()[1])
+
+    def sklearn_cosine_similarity(self, query, i):
+        """ calculate the cosine similarity between the query and each document with sklearn
+
+        :param query: the query
+        :type query: str
+        :param i: the index of the query
+        :type i: int
+        :return: the cosine similarity between the query and each document
+        :rtype: list
+        """
+
+        vectorizer = TfidfVectorizer(stop_words="english")
+        doc_query_text = list(self.collection.values()) + [query]
+        # print(doc_query_text[-1:])
+        tfidf = vectorizer.fit_transform(doc_query_text)
+
+        doc_tf_idf = tfidf[:len(self.collection)]
+        query_tf_idf = tfidf[len(self.collection):]
+
+        cosine_sim_matrix = cosine_similarity(query_tf_idf[0], doc_tf_idf)
+        # Get the cosine similarity for query 1 and all documents
+        query_cosine_sims = cosine_sim_matrix[0]
+
+        # Create a hashmap from "docno" to "cosine_sim" for query 1
+        ranked_list = {
+            docno: cosine_sim
+            for docno, cosine_sim in zip(self.collection.keys(),
+                                         query_cosine_sims)
+        }
+
+        sorted_dict = sorted(ranked_list.items(),
+                                    key=lambda x: x[1],
+                                    reverse=True)
+
+        rank = 1
+        result = ""
+        for docno, score in sorted_dict:
+            if rank <= 1000:
+                result += f"{i}\tQ0\t{docno}\t{rank}\t{score}\tExp\n"
+                # result += str(i) + "Q0 " + docno + " " + str(rank) + " " + str(score) + " Exp\n"
+                rank += 1
+        print(f"Query {i} done")
+
+        return result
+
+
+    def get_inverted_index(self):
+        """ get the inverted index
+
+        :return: the inverted index
+        :rtype: dict
+        """
+
+        return self.inverted_index
+
+    def save_inverted_index(self, file_path):
+        """ save the inverted index to a file
+
+        :param file_path: the file path to save the inverted index
+        :type file_path: str
+        :return: None
+        """
+
+        with open(file_path, "w") as f:
+            for term in self.inverted_index:
+                f.write(term + ": ")
+                for docno in self.inverted_index[term]:
+                    f.write(docno + " " +
+                            str(self.inverted_index[term][docno]) + " ")
+                f.write("\n")
+
+    def calculate_bert_similarity_score(self):
+        # Check if CUDA is available, and set the device accordingly
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+        # Load roberta-large
+        model = RobertaModel.from_pretrained("roberta-large").to(device)
+        tokenizer = RobertaTokenizer.from_pretrained("roberta-large")
+
+        # Set the model to evaluation mode
+        model.eval()
+
+        max_sequence_length = 512
+        # run 50 query takes too much time, select only query from '1' to '5' for testing
+        self.queries = {k: v for k, v in self.queries.items() if k in ['1', '2', '3', '4', '5']}
+
+        # Prepare a file to write results in trec_eval format
+        with open("trec_eval.txt", "w") as f:
+            # Calculate the similarity score for each document
+            for query_id, query in tqdm(self.queries.items(), desc="Processing queries"):
+                # Tokenize and encode the query
+                query_ids = tokenizer.encode(query, add_special_tokens=True, return_tensors='pt', max_length=max_sequence_length, truncation=True).to(device)
+
+                # Get the embeddings of the query
+                with torch.no_grad():
+                    query_embeddings = model(query_ids)[0]
+
+                # Average the embeddings of tokens in the query
+                query_embeddings = torch.mean(query_embeddings, dim=1)
+
+                # Calculate the similarity score for each document
+                for docno, doc in tqdm(self.collection.items(), desc="Processing documents", leave=False):
+                    # Tokenize and encode the document
+                    doc_ids = tokenizer.encode(doc, add_special_tokens=True, return_tensors='pt', max_length=max_sequence_length, truncation=True).to(device)
+
+                    # Get the embeddings of the document
+                    with torch.no_grad():
+                        doc_embeddings = model(doc_ids)[0]
+
+                    # Average the embeddings of tokens in the document
+                    doc_embeddings = torch.mean(doc_embeddings, dim=1)
+
+                    # Calculate the similarity score
+                    score = nn.functional.cosine_similarity(query_embeddings, doc_embeddings, dim=-1).item()
+
+                    # Write the result to the file in trec_eval format
+                    f.write(f"{query_id}\tQ0\t{docno}\t1\t{score}\tExp\n")
+    def get_cosine_similarity(self, query):
+        '''Calculates the cosine similarity between the query and each document.
+
+        :param query: the tokenized query
+        :type query: set
+        :return: ranked list of documented in reversed order of their relevance.
+        :rtype: list
+        '''
+        ''''''
+        # print(query)
+
+        # calculate the query vector
+        #   structure: term -> tf
+        #   example: "the" -> 2
+        query_vector = {}
+        for term in query:
+            if term not in query_vector:
+                query_vector[term] = 1
+            else:
+                query_vector[term] += 1
+
+        # calculate the document vector
+        #   structure: docno -> {term -> tf-idf}
+        #   example: d0 -> {"the" -> 0.1, "is" -> 0.05}
+        document_vectors = {}
+        for term in query_vector:
+            if term in self.inverted_index:
+                for docno in self.inverted_index[term]:
+                    if docno not in document_vectors:
+                        document_vectors[docno] = {}
+                    document_vectors[docno][term] = self.inverted_index[term][
+                        docno]
+
+        # calculate the cosine similarity
+        cosine_similarities = {}
+        for docno in document_vectors:
+            cosine_similarities[docno] = 0
+            for term in query_vector:
+                if term in document_vectors[docno]:
+                    cosine_similarities[docno] += query_vector[
+                        term] * document_vectors[docno][term]
+
+        # normalize the cosine similarity
+        '''
+        for docno in cosine_similarities:
+            cosine_similarities[docno] /= (math.sqrt(
+                len(query_vector) * len(document_vectors[docno])))
+        '''
+        sum1 = 0
+        sum2 = 0
+        for docno in document_vectors:
+            for term in query_vector:
+                if term in document_vectors[docno]:
+                    sum1 += math.pow(query_vector[term], 2)
+                    sum2 += math.pow(document_vectors[docno][term], 2)
+
+            sum1 = math.sqrt(sum1)
+            sum2 = math.sqrt(sum2)
+            cosine_similarities[docno] /= (sum1 * sum2)
+
+        # sort the cosine similarity
+        cosine_similarities = sorted(cosine_similarities.items(),
+                                     key=lambda x: x[1],
+                                     reverse=True)
+
+        return cosine_similarities
+
+    def calculate_result(self):
+        ''' Calculate the result of the queries with multiprocessing
+        '''
+
+        # multiprocessing
+        pool = Pool()
+        process_list = []
+
+        self.stats = 0
+        for i in range(1, self.Q + 1):
+            process_list.append(
+                pool.apply_async(self.calculate_bert_similarity_score,
+                                (self.queries[str(i)], )))
+        pool.close()
+        pool.join()
+
+        # write to file
+        with open("results.txt", "w") as f:
+            for i in range(len(process_list)):
+                f.write(process_list[i].get())
+
+if __name__ == '__main__':
+    # limit memory usage
+    # resource.setrlimit(resource.RLIMIT_AS, (1024 * 1024 * 1024, -1))
+
+    # Check for GPU availability and set the device
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f"Using device: {device}")
+
+    # irs = IRSystem("./collection.txt", "./topics1-50.txt")
+    irs = IRSystem("./Collection", "./topics1-50.txt")
+    print("Calculating...")
+    irs.calculate_bert_similarity_score()
+    
+    # load similarity scores from file
+    # similarity_scores = {}
+    # with open("similarity_scores.txt", "r") as f:
+    #     for line in f:
+    #         queryno, docno, similarity = line.split()
+    #         if queryno not in similarity_scores:
+    #             similarity_scores[queryno] = {}
+    #         similarity_scores[queryno][docno] = float(similarity)
+
+
+    # irs.save_inverted_index("./inverted_index.txt")
+    #print(irs.queries)
+    #print(irs.queries["1"])
+    # result = ""
+    # rank = 1
+    # with open('results.txt', 'w') as f:
+    #     for queryno, scores in similarity_scores.items():
+    #         rank = 1
+    #         for docno, similarity in scores.items():
+    #             result = f"{queryno} Q0 {docno} {rank} {similarity} bert\n"
+    #             f.write(result)
+    #             rank += 1
+
+
+    # print(irs.get_cosine_similarity(irs.queries["1"]))
+    #     for i in range(1, len(irs.queries) + 1):
+    #         # ranked_list = irs.get_cosine_similarity(irs.queries[str(i)])
+    #         ranked_list = irs.sklearn_cosine_similarity(irs.queries[str(i)])
+    #         sorted_dict = sorted(ranked_list.items(),
+    #                                     key=lambda x: x[1],
+    #                                     reverse=True)
+    #         for docno, score in sorted_dict:
+    #             result += str(i) + " Q0 " + docno + " " + str(rank) + " " + str(score) + " Exp\n"
+    #             rank += 1
+    #         rank = 1
+    #     f.write(result)
+    #
+
+            # for j in range(len(ranked_list)):
+            #     if j == 1001:
+            #         break
+            #     else:
+            #         row = "{}\tQ0\t{}\t{}\t{}\tExp\n".format(
+            #             i, ranked_list[j][0], rank, ranked_list[j][1])
+            #         # row = str(i) + " Q0 " + str(
+            #         #     ranked_list[j][0]) + " " + str(rank) + " " + str(
+            #         #         ranked_list[j][1]) + " run_name" + "\n"
+            #         rank += 1
+            #         f.write(row)
+    # ranked_list = irs.get_cosine_similarity(irs.queries["1"])
+    # print(ranked_list[:10])
---
+++ A2/src/Extractor.py
@@ -0,0 +1,244 @@
+import os  # used to explore the file system
+import re  # used to search for the pattern
+import multiprocessing  # used to parallelize the extraction
+from nltk.stem import PorterStemmer  # find the root of the word
+
+
+class Extractor:
+
+    def __init__(self, collection_path, query_path):
+        """ Initialize the Extractor class with the collection and query paths
+
+        :param collection_path: path to the collection file
+        :type collection_path: str
+        :param query_path: path to the query file
+        :type query_path: str
+        """
+
+        self.collection_path = collection_path
+        self.query_path = query_path
+        self.queries = {}
+        self.load_query()
+        self.collection = {}
+
+        # if collection_path points to a file, load the collection from the file
+        if os.path.isfile(collection_path):
+            self.load_collection(collection_path)
+        else:
+            self.load_collection()
+
+    def tokenize(self, text) -> list[str]:
+        '''Tokenize the text
+
+        :param text: the text to tokenize
+        :type text: str
+        :return: the tokenized text
+        :rtype: list
+        '''
+
+        # def the pattern of the regex to filting
+        regex_pattern = r'\w+'
+        tokens_with_stopword = re.findall(regex_pattern,
+                                          text)  # tokenize the text
+
+        # print(tokens_with_stopword)
+
+        # use the stopwords provided by the prof
+        f = open('StopWords', 'r')
+        stop_words = f.read().splitlines()
+
+        # remove the stopwords
+        tokens_cleaned = [
+            w for w in tokens_with_stopword
+            if w.lower() not in stop_words and w.isalpha()
+        ]
+
+        # stem word using PorterStemmer
+        ps = PorterStemmer()
+        tokens_stemmed = [ps.stem(w) for w in tokens_cleaned]
+
+        return tokens_stemmed
+
+    def parse_file(self, filename):
+        """ parse the given file and extract the DOCNO and TEXT
+
+        :param filename: the file to parse
+        :type filename: str
+        :return: a hashmap of the document number and inverted index
+        :rtype: dict
+        """
+        # Old code to parse the file by using regex
+        #     # Read the contents of the file
+        #     buffer = f.read()
+        #     # Extract doc tags from the file by using regex
+        #     doc = self.extract_tag('DOC', buffer)
+        #     # Extract docno and text tags from the file by using regex
+        #     for d in doc:
+        #         # !!! Assume there always has a space after <docno> and before </docno> !!!
+        #         docno = self.extract_tag('DOCNO', d)[0]
+        #         text = ' '.join(self.extract_tag('TEXT', d))
+        #         # Tokenize the text
+        #         tokens = Tokenizer.tokenize(text)
+        #         # Add the docno and tokenized text to the hashmap
+        #         doc_text_map[docno] = tokens
+
+        print(filename)
+
+        with open(os.path.join(self.collection_path, filename), 'r') as file:
+
+            flag_doc = False  # indicate whether the current line is in a doc
+            flag_text = False  # indicate whether the current line is in a text
+            docno = ""
+            text = ""
+            collection = {}
+
+            for line in file:
+                # print(line)
+                if "<DOC>" in line:
+                    if flag_doc:
+                        raise Exception("Error: <DOC> tag is not closed")
+                    flag_doc = True
+                if "<DOCNO>" in line:
+                    if not flag_doc:
+                        raise Exception("Error: <DOC> tag is not opened")
+                    if flag_text:
+                        raise Exception("Error: <TEXT> tag is not closed")
+                    docno = re.findall(r'<DOCNO> (.*?) </DOCNO>', line)[0]
+                if "</TEXT>" in line:
+                    if not flag_doc:
+                        raise Exception("Error: <DOC> tag is not opened")
+                    flag_text = False
+                elif "<TEXT>" in line:
+                    if not flag_doc:
+                        raise Exception("Error: <DOC> tag is not opened")
+                    if flag_text:
+                        raise Exception("Error: <TEXT> tag is not closed")
+                    flag_text = True
+                elif flag_text:
+                    text += line
+                if "</DOC>" in line:
+                    if not flag_doc:
+                        raise Exception("Error: </DOC> tag is not opened")
+                    flag_doc = False
+                    # tokens = self.tokenize(text)
+                    # collection[docno] = tokens
+                    collection[docno] = text
+                    docno = ""
+                    text = ""
+
+        return collection
+
+    def load_collection(self, output_path=None):
+        """ Load all files from the collection path, map the docno and tokens
+
+        :return: a hashmap of the document number and tokens
+        :rtype: dict
+        """
+
+        if output_path is not None:
+            if not os.path.exists(output_path):
+                raise Exception("Error: the output path does not exist")
+            # self.collection = self.load_collection_from_file(output_path)
+            self.collection = self.load_collection_text_from_file(output_path)
+        else:
+            pool = multiprocessing.Pool()
+            process_list = []
+
+            # Iterate through the files in the collection path
+            for filename in os.listdir(self.collection_path):
+                # if filename != "AP880819":
+                #     continue
+                process_list.append(
+                    pool.apply_async(self.parse_file, args=(filename, )))
+
+            pool.close()
+            pool.join()
+
+            for process in process_list:
+                self.collection.update(process.get())
+
+    def load_query(self):
+        """ Load the file from the query path, map the query number and desc
+
+        :return: a hashmap of the query number and desc
+        :rtype: dict
+        """
+
+        # Open the file and read the contents
+        with open(self.query_path, 'r') as f:
+            # Read the contents of the file
+            query = re.findall(r'<top>(.*?)</top>', f.read(),
+                               re.MULTILINE | re.DOTALL)
+
+            for q in query:
+                # Extract query number and title from the file by using regex
+                queryno = re.findall(r'<num>(.*?)\n', q)
+                title = re.findall(r'<title>(.*?)\n', q)
+                desc = re.findall(r'<desc>(.*?)<narr>', q, re.DOTALL | re.MULTILINE)
+                query = title[0] + " " + desc[0]
+                # remove \n in the query
+                query = query.replace("\n", " ")
+                # Tokenize the title
+                # tokens = self.tokenize(title[0])
+                # Add the query number and tokenized title to the hashmap
+                
+                # self.queries[queryno[0].strip()] = tokens
+                self.queries[queryno[0].strip()] = query
+
+    def save_collection(self, output_path):
+        """ Save the collection to the given path
+
+        :param output_path: the path to save the collection
+        :type output_path: str
+        """
+
+        with open(output_path, 'w') as f:
+            for docno, tokens in self.collection.items():
+                f.write(docno + "\t" + " ".join(tokens) + "\n")
+
+    def load_collection_from_file(self, input_path):
+        """ Load the collection from the given path
+
+        :param input_path: the path to load the collection
+        :type input_path: str
+        :return: a hashmap of the document number and tokens
+        :rtype: dict
+        """
+
+        collection = {}
+
+        with open(input_path, 'r') as f:
+            for line in f:
+                docno, tokens = line.split("\t", 1)
+                collection[docno] = tokens.split()
+
+        return collection
+
+    def get_collection(self):
+        """ Get the collection
+
+        :return: a hashmap of the document number and tokens
+        :rtype: dict
+        """
+
+        return self.collection
+
+    def get_queries(self):
+        """ Get the query
+
+        :return: a hashmap of the query number and tokens
+        :rtype: dict
+        """
+
+        return self.queries
+
+
+if __name__ == '__main__':
+    extractor = Extractor('./Collection/', './topics1-50.txt')
+    # extractor.save_collection_text('./collection.txt')
+    #
+    # extractor = Extractor('./collection.txt', './topics1-50.txt')
+
+    # print(extractor.collection["AP881001-0003"])
+    for query in extractor.queries:
+        print(query, extractor.queries[query])
